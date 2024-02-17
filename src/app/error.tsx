3a93ae3b5a0cc18a724fc64cba21bc32ba6c63d9
Riccardo Consolandi <rconsolandi@gmail.com>
2024-02-17 15:20:01 +0100

    refactor: adjust error page, insert console.log for any error for the errorPage

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,11 +3,12 @@
 import { Button, Link } from "@mui/material";
 
 const error = ({ error, reset }: { error: Error; reset: () => void }) => {
+  console.log(error.message);
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <div>
         <p>There was a problem</p>
-        <h1>{error.message || "Something went wrong"}</h1>
+        <h1>Something went wrong</h1>
         <p>Please try again later...</p>
         <Button onClick={reset}>Try again</Button>
         <Link style={{ textDecoration: "none", color: "black", display: "block", marginTop: "10px" }} href="/">
